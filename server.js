debc7d357d08b50ec3c2505233db9d346e4dff62
OgabekYuldoshev <yuldashoff1gmail.com>
2021-11-20 17:59:45 +0500

    'sfsdf'

--- server.js
+++ server.js
@@ -1,61 +1,88 @@
-const express = require('express');
-const cors = require('cors');
-const ytdl = require('ytdl-core');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const path = require("path");
 const app = express();
-const slugify = require('slugify')
+const slugify = require("slugify");
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/get_info', async (req, res) => {
-    let URL = req.query.URL;
-    let data = await ytdl.getInfo(URL)
-    res.status(200).send({
+app.get("/", (req, res) => {
+  res.render("youtube", {
+    error: null,
+    video: null,
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    let data = await ytdl.getInfo(req.body.LINK);
+    res.render("youtube", {
+      error: null,
+      video: {
         name: data.videoDetails.title,
-        img: data.videoDetails.thumbnails.reverse()[0].url
-    })
-})
+        img: data.videoDetails.thumbnails.reverse()[0].url,
+        url: req.body.LINK,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("youtube", {
+      error: "Xatolik",
+      video: null,
+    });
+  }
+});
 
-app.get('/download_video', (req, res)=>{ 
-      let URL = req.query.URL;
-      let name = req.query.name;
-      let generate = slugify(name, {
-          replacement: '-', 
-          remove: undefined, 
-          lower: false,     
-          strict: false,     
-          locale: 'vi'       
-        })
-      res.setHeader('Content-Disposition', `attachment; filename="anonym-${generate}.mp4"`)
-      const video = ytdl(URL, {
-          quality: 'highestvideo',
-          filter: 'videoandaudio',
-          format: 'mp4'
-      })
-      video.pipe(res)
-})
+app.get("/video", (req, res) => {
+  let URL = req.query.LINK;
+  let name = req.query.name;
+  let generate = slugify(name, {
+    replacement: "-",
+    remove: undefined,
+    lower: false,
+    strict: false,
+    locale: "vi",
+  });
+  res.setHeader(
+    "Content-Disposition",
+    `attachment; filename="${generate}-download.mp4"`
+  );
+  const video = ytdl(URL, {
+    quality: "highestvideo",
+    filter: "videoandaudio",
+    format: "mp4",
+  });
+  video.pipe(res);
+});
 
-app.get('/download_audio', (req, res)=>{ 
-    let URL = req.query.URL;
-    let name = req.query.name;
-    let generate = slugify(name, {
-        replacement: '-', 
-        remove: undefined, 
-        lower: false,     
-        strict: false,     
-        locale: 'vi'       
-      })
-    res.setHeader('Content-Disposition', `attachment; filename="anonym-${generate}.mp3"`)
-    const audio = ytdl(URL, {
-        quality: 'highestaudio',
-        filter: "audioonly",
-        format: 'mp3'
-    })
-    audio.pipe(res)
-})
+app.get("/audio", (req, res) => {
+  let URL = req.query.LINK;
+  let name = req.query.name;
+  let generate = slugify(name, {
+    replacement: "-",
+    remove: undefined,
+    lower: false,
+    strict: false,
+    locale: "vi",
+  });
+  res.setHeader(
+    "Content-Disposition",
+    `attachment; filename="${generate}-download.mp3"`
+  );
+  const audio = ytdl(URL, {
+    quality: "highestaudio",
+    filter: "audioonly",
+    format: "mp3",
+  });
+  audio.pipe(res);
+});
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log('Server Works !!!');
+  console.log("Server Works !!!");
 });
